398098941c2b8d7c6b265334ed07ebb35a04ccb4
awnishd <121890475+awnishd@users.noreply.github.com>
2023-02-09 18:35:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import { Server } from 'socket.io';
+import dotenv from 'dotenv'
+dotenv.config();
+const PORT=9000 || process.env.PORT;
+const io = new Server(PORT, {
+    cors: {
+        origin: 'http://localhost:3000',
+    },
+})
+
+
+let users = [];
+
+const addUser = (userData, socketId) => {
+    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
+}
+
+
+
+const getUser = (userId) => {
+    return users.find(user => user.sub === userId);
+}
+
+
+
+
+//console.log(users);
+io.on('connection', (socket) => {
+    console.log('user connected')
+
+
+    socket.on("addUser", userData => {
+        addUser(userData, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    socket.on('sendMessage', (data) => {
+        const user = getUser(data.receiverId);
+        io.to(user.socketId).emit('getMessage', data)
+    })
+
+})
